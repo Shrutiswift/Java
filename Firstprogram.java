957295d7d91b0d469df971e6eab3d9945eb1ae25
Shruti <shrutiswift@gmail.com>
2022-05-27 22:31:05 +0530

    Hello

--- Firstprogram.java
+++ Firstprogram.java
@@ -1,5 +1,5 @@
-public class Firstprogram{
+private class Firstprogram{
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.print("Hello");
     }
 }
