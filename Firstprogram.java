ef381a6ce0745afd31980b653870e836cc81cded
Shruti <shrutiswift@gmail.com>
2022-05-30 22:09:05 +0530

    Adding

--- Firstprogram.java
+++ Firstprogram.java
@@ -2,8 +2,7 @@ public class Firstprogram{
     public static void main(String[] args) {
         int f = 3;
         int L = 8;
-        int n = f + L;
-        System.out.print(n);
+        System.out.print(f+L);
     
     }
 }
