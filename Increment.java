c58cfcd9fcacedda8e464ce419234be6026043db
Shruti <shrutiswift@gmail.com>
2022-05-31 23:33:10 +0530

    Increment

---
+++ Increment.java
@@ -0,0 +1,12 @@
+
+public class Increment {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = ++x;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
+
+
