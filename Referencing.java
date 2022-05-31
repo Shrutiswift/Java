273251ef2f1405dc39e04b4ee22b121a59909de2
Shruti <shrutiswift@gmail.com>
2022-05-31 16:03:03 +0530

    reference

---
+++ Referencing.java
@@ -0,0 +1,8 @@
+public class Referencing {
+    public static void main(String[] args) {
+        String message = "Hello World" + "!";
+        System.out.println(message.endsWith("!"));
+    }
+}
+
+
