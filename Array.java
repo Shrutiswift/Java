60b4b619b9f22524f61597358f18c321f01905e1
Shruti <shrutiswift@gmail.com>
2022-05-31 21:11:13 +0530

    Sort the array

--- Array.java
+++ Array.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
         byte[] numbers = {1, 8, 9, 5, 4, 34};
-        System.out.println(numbers.length);
+        Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
 }
