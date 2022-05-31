04aa9ed8c0a735ebfd2a18f1323a52c05c570790
Shruti <shrutiswift@gmail.com>
2022-05-31 16:03:47 +0530

    Length

--- Referencing.java
+++ Referencing.java
@@ -1,7 +1,7 @@
 public class Referencing {
     public static void main(String[] args) {
         String message = "Hello World" + "!";
-        System.out.println(message.endsWith("!"));
+        System.out.println(message.length());
     }
 }
 
