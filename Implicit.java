6fe74ee6be8ea7ebec37060a8ecf3d2770dac3a7
Shruti <shrutiswift@gmail.com>
2022-05-31 23:53:33 +0530

    implicit

--- Implicit.java
+++ Implicit.java
@@ -1,8 +1,8 @@
-
+// byte > short > int > long > float > double
 public class Implicit {
     public static void main(String[] args) {
         double x = 2.0;
-        int y = (int)x + 2;
+        double y = x + 2;
         System.out.println(y);
 
     }
