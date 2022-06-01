010f5653023eefdad034daf29bae74191586385a
Shruti <shrutiswift@gmail.com>
2022-06-01 23:22:20 +0530

    changes

--- Formatting.java
+++ Formatting.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Formatting {
+import java.text.NumberFormat;
+
+public class Formatting {
+    public static void main(String[] args) {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(12345677.98765);
+        System.out.println(result);
+    }
 }
