db1a79f6b631aa9ed2d1cc80875072faffbd7847
Shruti <shrutiswift@gmail.com>
2022-06-01 17:53:25 +0530

    First git commit through IntelliJ

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import static java.lang.System.out;
+import static java.sql.DriverManager.println;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
