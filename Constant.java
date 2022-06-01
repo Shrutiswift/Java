058d57ff137df376665d575da3d42eee0351e7bb
Shruti <shrutiswift@gmail.com>
2022-06-01 14:33:58 +0530

    implicit

--- Constant.java
+++ Constant.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Constant {
     public static void main(String[] args) {
         final float PI = 3.14f;
--- Implicit.java
+++ Implicit.java
@@ -1,11 +1,16 @@
+import java.text.DecimalFormat;
+
 // byte > short > int > long > float > double
 public class Implicit {
     public static void main(String[] args) {
-        double x = 2.0;
-        double y = x + 2;
+        String x = "1.123121";
+        double y = Double.parseDouble(x) + 2;
         System.out.println(y);
-
+        DecimalFormat df = new DecimalFormat("#.####");
+        System.out.println(Double.MAX_VALUE); 
+        System.out.println(df.format(y));
     }
 }
+
 
 
