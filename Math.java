f4250ba7b96fda358b77a5144ea4b63827720614
Shruti <shrutiswift@gmail.com>
2022-06-01 16:02:24 +0530

    Math

---
+++ Math.java
@@ -0,0 +1,7 @@
+import static java.lang.Math.sqrt;
+public class Math {
+    public static void main(String[] args){
+        int result = (int)Math.ceil(4.5F);
+        System.out.println(result);
+    } 
+}
