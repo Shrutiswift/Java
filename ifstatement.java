a4a884127d0160bfafb1c756cd9d8a6fedf2accd
Shruti <shrutiswift@gmail.com>
2022-06-02 21:07:05 +0530

    Checking statements using if

---
+++ ifstatement.java
@@ -0,0 +1,11 @@
+public class ifstatement {
+    public static void main(String[] args) {
+        int temp = 23;
+        if (temp == 65)
+        {
+            System.out.println("Its a hot day!");
+        } else if (temp > 20 && temp  <= 30){
+            System.out.println("Its a good day!");
+        }
+    }
+}
