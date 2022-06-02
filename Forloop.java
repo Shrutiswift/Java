0808e8b6e89aacc93bdd53e97d68a6e9db4f7109
Shruti <shrutiswift@gmail.com>
2022-06-02 22:32:59 +0530

    Print hello world using for loop

--- Forloop.java
+++ Forloop.java
@@ -2,5 +2,11 @@ public class Forloop {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++)
         System.out.println("Hello World");
+
+        int i = 0;
+        while(i > 0) {
+            System.out.println("Hello World");
+            i--;
+        }
     }
 }
