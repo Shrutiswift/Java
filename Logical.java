2058bee7922fc79f031694edad41fbfc59eba2a6
Shruti <shrutiswift@gmail.com>
2022-06-03 14:10:57 +0530

    Using || and &&

--- Logical.java
+++ Logical.java
@@ -2,7 +2,8 @@ public class Logical {
     public static void main(String[] args) {
         boolean hasGoodIncome = true;
         boolean hasGoodCredit = true;
-        boolean isEligible = hasGoodCredit || hasGoodIncome;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasGoodCredit || hasGoodIncome) && hasCriminalRecord;
         System.out.println(isEligible);
     }
 }
