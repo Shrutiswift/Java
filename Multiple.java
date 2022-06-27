5e64fabc1ad16951dfb8cf4029ef0100ccbafdac
Shruti <shrutiswift@gmail.com>
2022-06-27 18:00:45 +0530

    Taking input from user and display it.

---
+++ Multiple.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Multiple{
+    int age;
+    String name;
+    public Multiple(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+    public Multiple() {
+
+    }
+    public void display() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.next();
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+
+    }
+    public static void main(String[] args) {
+        Multiple object = new Multiple();
+        object.display();
+    }
+}
+
+
+
+
