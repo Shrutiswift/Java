049f81ac77224b0cef2bbae838f28bd6df8100df
Shruti <shrutiswift@gmail.com>
2022-06-28 13:14:53 +0530

    default constructor.

---
+++ Default.java
@@ -0,0 +1,8 @@
+public class Default {
+    Default(){
+        System.out.println("This is a default constructor");
+    }
+    public static void main(String[] args) {
+        Default obj = new Default();
+    }
+}
