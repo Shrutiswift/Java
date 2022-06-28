3d9f6b90c85cb4bfa144b4581d211c55afd52449
Shruti <shrutiswift@gmail.com>
2022-06-28 21:32:48 +0530

    Arithmetic operators

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 package src;
-class GFG {
 
-    static String geek;
+import java.util.Scanner;
 
-    public static void main(String[] args)
-    {
+class calculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        byte first = input.nextByte();
+        byte second = input.nextByte();
+        System.out.println("Enter the operator: ");
+        char operator = input.next().charAt(0);
+        byte result;
+        switch(operator) {
+            case '+':
+                result = (byte) (first + second);
+                break;
+            case '-':
+                result = (byte) (first - second);
+                break;
+            default:
+                System.out.println("Sorry!");
+                return;
+        }
+            System.out.println(result);
 
-        System.out.println("My name is: " + GFG.geek);
-    }
+        }
 }
