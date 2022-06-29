f65cc2870121325b8078b703f33c622e439b2168
Shruti <shrutiswift@gmail.com>
2022-06-29 13:46:57 +0530

    Multidimensional array

--- Array.java
+++ Array.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
-        byte[] numbers = {1, 8, 9, 5, 4, 34};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        String[][]names = {{"Shruti", "Veeru", "Jay"}, {"ME", "You", "Us"}};
+        System.out.println(names[0][1] + names[1][2]);
+//        byte[] numbers = {1, 8, 9, 5, 4, 34};
+//        Arrays.sort(numbers);
+//        System.out.println(Arrays.toString(numbers));
     }
 }
 
