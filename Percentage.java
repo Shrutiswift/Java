9c33ef5f9c0170ba318159c93adb61d6c7899cbb
Shruti <shrutiswift@gmail.com>
2022-06-30 12:24:21 +0530

    Calculate percentage

--- Percentage.java
+++ Percentage.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 public class Percentage {
-    static int marks;
-    static int percent_age;
-    static int a;
-    int total_score;
-    static int total = 0;
-    public void input(){
-        Scanner input = new Scanner (System.in);
+    private int marks;
+    private int percent;
+    private int numSubjects;
+    private int total;
+    void Percentage() {
+        total = 0;
+    }
+
+    public void input() {
+        Scanner input = new Scanner(System.in);
         System.out.println("How many subjects do you have? ");
-        int a = input.nextInt();
-        for(int i = 0; i < a; i++){
+        numSubjects = input.nextInt();
+        for (int i = 0; i < numSubjects; i++) {
             System.out.println("Enter the marks: ");
             marks = input.nextInt();
             total += marks;
         }
     }
-    public static int main(String[] args) {
+
+    public void calculate() {
+        percent = (total*100)/(numSubjects*100);
+    }
+
+    public static void main(String[] args) {
         Percentage obj = new Percentage();
         obj.input();
-        percent_age = (total*100) / (a*100);
-        System.out.println(obj.input(percent_age));
+        obj.calculate();
+        System.out.println("Total percentage: " + obj.percent);
     }
 }
