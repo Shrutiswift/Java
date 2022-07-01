9d4df8af5f885b6f45c476b1a84ac82758ab97ab
Shruti <shrutiswift@gmail.com>
2022-07-01 16:29:54 +0530

    Array

--- Array.java
+++ Array.java
@@ -1,7 +1,8 @@
-import java.util.Arrays;
+
 public class Array {
+    int [] arr = new int [10];
     public static void main(String[] args) {
-        String[][]names = {{"Shruti", "Veeru", "Jay"}, {"ME", "You", "Us"}};
+        String[][]names = {{"Brutish", "Veer", "Jay"}, {"ME", "You", "Us"}};
         System.out.println(names[0][1] + names[1][2]);
 //        byte[] numbers = {1, 8, 9, 5, 4, 34};
 //        Arrays.sort(numbers);
