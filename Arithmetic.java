6c5ca128f290a568e84c0a05851bb1aa3771dc9b
Shruti <shrutiswift@gmail.com>
2022-07-02 21:58:04 +0530

    Arithmetic

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,5 +1,5 @@
 
-public class Arithmetic {
+public class  Arithmetic {
     public static void main(String[] args) {
         double result = (double)12 / (double) 9;
         System.out.println(result);
