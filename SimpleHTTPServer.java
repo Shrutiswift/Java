6332fd0a5f2b5396f472d683d4fec201a1f3c942
Shruti <shrutiswift@gmail.com>
2022-07-04 15:37:47 +0530

    HTTP server

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,21 +1,26 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class SimpleHTTPServer {
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
             System.out.println("Listening for connection on port 8080....");
             while(true){
-                Socket clientSocket = server.accept();
-                InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-                BufferedReader reader = new BufferedReader(isr);
-                String line = reader.readLine();
-                while(!line.isEmpty()){
-                    System.out.println(line);
-                    line = reader.readLine();
+//                Socket clientSocket = server.accept();
+//                InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+//                BufferedReader reader = new BufferedReader(isr);
+//                String line = reader.readLine();
+//                while(!line.isEmpty()){
+//                    System.out.println(line);
+//                    line = reader.readLine();
+                try(Socket socket = server.accept()){
+                    Date today = new Date();
+                    String httpResponse = "Ok"+today;
+                    socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
                 }
             }
     }
