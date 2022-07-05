1638a4efe081b217c0ae62e27a3326fe01915370
Shruti <shrutiswift@gmail.com>
2022-07-05 21:39:33 +0530

    same integer with different strings

--- AddElementToHashtable.java
+++ AddElementToHashtable.java
@@ -4,7 +4,7 @@ public class AddElementToHashtable {
     public static void main(String[] args) {
         Hashtable<Integer,String> hs = new Hashtable<Integer,String>();
         hs.put(1, "Kaushal");
-        hs.put(2, "Kaushal");
+        hs.put(1, "Kaus");
             System.out.println(hs);
 
     }
