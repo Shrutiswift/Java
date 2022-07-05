0ec980cce05a2e9bf2817ec3da5d5b166c334155
Shruti <shrutiswift@gmail.com>
2022-07-05 22:04:48 +0530

    removed an element from hashtable

--- AddElementToHashtable.java
+++ AddElementToHashtable.java
@@ -3,9 +3,9 @@ import java.io.*;
 public class AddElementToHashtable {
     public static void main(String[] args) {
         Hashtable<Integer,String> hs = new Hashtable<Integer,String>();
-        hs.put(1, "Kaushal");
-        hs.put(1, "Kaus");
-            System.out.println(hs);
-
+        hs.put(1, "Kaushal"); //remove Kaushal
+        hs.put(2, "Kaus");
+        hs.remove(1);
+        System.out.println(hs);
     }
 }
