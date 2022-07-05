750ff93e821b3d9c3fbe666958f8e2538f57c40e
Shruti <shrutiswift@gmail.com>
2022-07-05 21:32:23 +0530

    Hashtable

---
+++ AddElementToHashtable.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class AddElementToHashtable {
+    public static void main(String[] args) {
+        Hashtable<Integer,String> hs = new Hashtable<Integer,String>();
+        hs.put(1, "Kaushal");
+            System.out.println(hs);
+
+    }
+}
