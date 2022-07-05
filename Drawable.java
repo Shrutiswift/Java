8aa8c4964247aec5ad3d29cbe7a5b8b049edcc1c
Shruti <shrutiswift@gmail.com>
2022-07-05 14:33:11 +0530

    Interface

--- Drawable.java
+++ Drawable.java
@@ -1,17 +1,4 @@
-
-class Rectangle implements Drawable {
-    public void draw(){
-        System.out.println("hsavhs");
-    }
-}
-class Default implements Drawable{
-    public void draw(){
-        System.out.println("Heyyy");
-    }
-}
-class Me{
-    public static void main(String[] args){
-        Drawable b = new Default();
-        b.draw();
-    }
-}
+interface Bicycle{
+    void applyspeed();
+    void rest();
+};
