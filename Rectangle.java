3f5f065f883dd4c719f7e8c7c674d1a7715cfc6b
Shruti <shrutiswift@gmail.com>
2022-07-05 14:32:59 +0530

    Interface

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,19 @@
-interface Drawable{
-    void draw();
-};
+class Rectangle implements Bicycle {
+    public void applyspeed() {
+        System.out.println("Increase the speed by 10%");
+    }
+
+    public void rest() {
+        System.out.println("Take rest");
+    }
+}
+class Drawable{
+    public static void main(String[] args) {
+        Rectangle obj = new Rectangle();
+        obj.applyspeed();
+        obj.rest();
+    }
+}
+
+
+
