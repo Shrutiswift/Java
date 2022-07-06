25729e30429d5b40906fbd863b5178137d4f41c6
Shruti <shrutiswift@gmail.com>
2022-07-06 11:34:01 +0530

    Adding element to vector

--- AddingElementsToVector.java
+++ AddingElementsToVector.java
@@ -3,14 +3,9 @@ import java.util.*;
 
 public class AddingElementsToVector {
     public static void main(String[] args) {
-        int n = 5;
-        Vector<Integer> vt = new Vector<Integer>(n);
-        for (int i = 0; i <= n; n++)
-            vt.add(i);
+        Vector<String> vt = new Vector<String>();
+        vt.add("add");
         System.out.println(vt);
-        vt.remove(3);
         System.out.println(vt);
-        for(int i = 0; i < vt.size(); i++)
-            System.out.println(vt.get(i) + " ");
     }
 }
