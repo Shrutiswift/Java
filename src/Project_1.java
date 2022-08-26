652563359751438d9ae78085fbdb0f6565d833f2
Shruti <75881545+Shrutiswift@users.noreply.github.com>
2022-08-26 23:40:15 +0530

    Update Project_1.java

--- src/Project_1.java
+++ src/Project_1.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Project_1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter number: ");
+        System.out.println("Enter number: ");
         int n = s.nextInt();
         for(int i = 1; i <= 10; i++){
             System.out.println(n + "*" + i + "=" + n*i);
